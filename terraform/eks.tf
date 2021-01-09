68030b8d215f51caa8dbf4bc147a3d9f1bb055fe
nzewiski <nzewiski@yahoo.com>
2021-01-09 16:01:45 -0700

    add_bastion_access

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,6 +5,12 @@ module "eks-cluster" {
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
+  map_roles = [
+    rolearn = "arn:aws:iam::672499893721:role/SSMManagedInstanceCore"
+    username = "Bastion"
+    groups = "system:masters"
+  ]
+
   cluster_enabled_log_types = [
     "api",
     "audit",
