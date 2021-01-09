229e6aa150d539f6d73f7f6f97887c37b465e4a2
nzewiski <nzewiski@yahoo.com>
2021-01-09 16:28:39 -0700

    even_more_s3_restrictions

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -22,4 +22,5 @@ resource "aws_s3_bucket_public_access_block" "block" {
   restrict_public_buckets = true
   block_public_acls       = true
   block_public_policy     = true
+  ignore_public_acls      = true
 }
