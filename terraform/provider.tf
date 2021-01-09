8d96728b4868e90ab2bbdb6ddade81b0736574db
nzewiski <nzewiski@yahoo.com>
2021-01-09 12:57:36 -0700

    provider_update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,3 +6,11 @@ provider "aws" {
     role_arn     = "arn:aws:iam::672499893721:role/svc_terraform"
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-672499893721"
+    key    = "aetna-challenge"
+    region = "us-east-1"
+  }
+}
