ea12c7ffb97d202ff9ab71dd0944d97bc5adb05d
nzewiski <nzewiski@yahoo.com>
2021-01-10 23:59:54 -0700

    add_private_access_to_eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,9 +1,11 @@
 module "eks-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "challenge-cluster"
-  cluster_version = "1.17"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  source                          = "terraform-aws-modules/eks/aws"
+  cluster_name                    = "challenge-cluster"
+  cluster_version                 = "1.17"
+  subnets                         = module.vpc.private_subnets
+  vpc_id                          = module.vpc.vpc_id
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
   map_roles = [
     {
