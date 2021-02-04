2f4a54ec46c7c060832e7c985d21d62747f34525
Nick Zewiski <nzewiski@yahoo.com>
2021-02-04 19:29:23 +0000

    run tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,7 +1,7 @@
 module "eks-cluster" {
   source                          = "terraform-aws-modules/eks/aws"
   cluster_name                    = "challenge-cluster"
-  cluster_version                 = "1.17"
+  cluster_version                 = "1.18"
   subnets                         = module.vpc.private_subnets
   vpc_id                          = module.vpc.vpc_id
   cluster_endpoint_private_access = true
